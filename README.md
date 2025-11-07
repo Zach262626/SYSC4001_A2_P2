3241d4d962f407e499908c28f6ec75e64aa9be29
Zachary <zachgallant26@gmail.com>
2025-11-06 23:12:49 -0500

    Question 1

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# SYSC4001_A2_P2
+# SYSC 4001 Assignment 2 Part 2
+
+# Question 1
+    gcc -o part2_process part2_process_101272210_101316700.c
+    ./part2_process
+    ps -ef | grep part2_process
+    kill PID
---
+++ part2_process_101272210_101316700.c
@@ -0,0 +1,38 @@
+/**
+ * SYSC4001 Assignment 2
+ * Zachary Gallant 101272210
+ * Joseph Dereje 101316700
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+
+int main() {
+    int counter;
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        fprintf(stderr, "Fork failed\n");
+        return 1;
+    } else if (pid == 0) {
+        counter = 0;
+        printf("Child started PID: %d\n", getpid());
+        while(1) {
+            printf("Child Counter: %d\n", counter);
+            counter++;
+            sleep(1);
+        }
+    } else {
+        counter = 0;
+        printf("Parent PID: %d created child PID: %d\n", getpid(), pid);
+        while(1) {
+            printf("Parent Counter: %d\n", counter);
+            counter++;
+            sleep(1);
+        }
+    }
+
+    return 0;
+}
